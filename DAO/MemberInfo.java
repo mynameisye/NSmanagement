a22272bf87ecfb060c1b2c1657836677f6fe3770
mynameisye <102737288+mynameisye@users.noreply.github.com>
2023-12-02 22:10:55 +0900

    Add files via upload

---
+++ DAO/MemberInfo.java
@@ -0,0 +1,92 @@
+package model;
+
+import java.sql.Date;
+
+public class MemberInfo {
+	private int MemId;
+	private float Height;
+    private float Weight;
+    private String Disease;
+    private int Smoke;
+    private int Alchol;
+    private Date Pregnant;
+    private String Medicine;
+    private String Supplement;
+    private String PrefIngredient;
+    
+    public MemberInfo(int memId, float height, float weight, String disease, int smoke, int alchol, 
+    		Date pregnant, String medicine, String supplement, String prefIngredient) {
+        super();
+        MemId = memId;
+        Height = height;
+        Weight = weight;
+        Disease = disease;
+        Smoke = smoke;
+        Alchol = alchol;
+        Pregnant = pregnant;
+        Medicine = medicine;
+        Supplement = supplement;
+        PrefIngredient = prefIngredient;
+    }
+    
+    public int getMemId() {
+        return MemId;
+    }
+    public void setMemId(int memId) {
+    	MemId = memId;
+    }
+    public float getHeight() {
+        return Height;
+    }
+    public void setHeight(float height) {
+    	Height = height;
+    }
+    public float getWeight() {
+        return Weight;
+    }
+    public void setWeight(float weight) {
+    	Weight = weight;
+    }
+    public String getDisease() {
+    	return Disease;
+    }
+    public void setDisease(String disease) {
+    	Disease = disease;
+    }
+    public int getSmoke() {
+        return Smoke;
+    }
+    public void setSmoke(int smoke) {
+        Smoke = smoke;
+    }
+    public int getAlchol() {
+        return Alchol;
+    }
+    public void setSupId(int alchol) {
+    	Alchol = alchol;
+    }
+    public Date getPregnant() {
+    	return Pregnant;
+    }
+    public void setPregnant(Date pregnant ) {
+    	Pregnant = pregnant;
+    }
+    public String getMedicine() {
+    	return Medicine;
+    }
+    public void setMedicine(String medicine) {
+    	Medicine = medicine;
+    }
+    public String getSupplement() {
+    	return Supplement;
+    }
+    public void setSupplement(String supplement) {
+    	Supplement = supplement;
+    }
+    public String getPrefIngredient() {
+    	return PrefIngredient;
+    }
+    public void setPrefIngredient(String prefIngredient) {
+    	PrefIngredient = prefIngredient;
+    }
+}
---
+++ DAO/UserDao.java
@@ -0,0 +1,146 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import util.JDBCUtil;
+
+public class UserDao {
+    private JDBCUtil jdbcUtil = null;
+    
+    public UserDao() {
+        jdbcUtil = new JDBCUtil();
+    };
+    
+    //유저 가입(생성)
+    public int addUser(User user) {
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO Member ");
+        query.append("VALUES (?, ?, ?, ?, ?, ?, ?) ");
+        
+        jdbcUtil.setSqlAndParameters(query.toString(), 
+                new Object[] {"mem_seq.nextval",
+                        user.getId(),
+                        user.getPw(),
+                        user.getName(),
+                        user.getPhone(),
+                        user.getBirthDay().toString(),
+                        user.getEmail()
+                        });
+        try {
+            int result = jdbcUtil.executeUpdate();
+            return result;
+        } catch (Exception ex) {
+            jdbcUtil.rollback();
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.commit();
+            jdbcUtil.close();
+        }
+        
+        return 0;
+    }
+    
+    //유저 정보 갱신
+    public int updateUser(User user) throws SQLException{
+        StringBuilder query = new StringBuilder();
+        query.append("UPDATE Member ");
+        query.append("SET pwd=?, name=?, phone=?, birthday=?, email=? ");
+        query.append("WHERE MemId = ?");
+        
+        jdbcUtil.setSqlAndParameters(query.toString(), 
+                new Object[] {
+                        user.getPw(),
+                        user.getName(),
+                        user.getPhone(),
+                        user.getBirthDay().toString(),
+                        user.getEmail(),
+                        user.getMemid()});
+        try {
+            int result = jdbcUtil.executeUpdate();
+            return result;
+        } catch (Exception ex) {
+            jdbcUtil.rollback();
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.commit();
+            jdbcUtil.close();
+        }
+        
+        return 0;
+    }
+    
+    //유저 삭제
+    public int deleteUser(String id) throws SQLException{
+        StringBuilder query = new StringBuilder();
+        query.append("DELETE FROM Member ");
+        query.append("WHERE MemId = ?");
+        jdbcUtil.setSqlAndParameters(query.toString(), new Object[] {id});
+        try {
+            int result = jdbcUtil.executeUpdate();
+            return result;
+        } catch (Exception ex){
+            jdbcUtil.rollback();
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.commit();
+            jdbcUtil.close();
+        }
+        
+        return 0;
+    }
+    
+    //유저 정보 조회
+    public User findUser(String id) throws SQLException{
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT memid, pwd, name, gender, phone, birthday, email ");
+        query.append("FROM Member ");
+        query.append("WHERE MemId = ?");
+        jdbcUtil.setSqlAndParameters(query.toString(), new Object[] {id});
+        try {
+            ResultSet rs = jdbcUtil.executeQuery();
+            if(rs.next()) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate birth = LocalDate.parse(rs.getString("birthday"), formatter);
+                User user = new User(
+                        rs.getInt("memid"),
+                        id,
+                        rs.getString("pwd"),
+                        rs.getString("name"),
+                        rs.getString("gender").charAt(0),
+                        rs.getString("phone"),
+                        birth,
+                        rs.getString("email"));
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.close();
+        }
+        
+        return null;
+    }
+    
+    //존재하는 유저 id인지 검사
+    public boolean existingUser(String userId) throws SQLException{
+        StringBuilder query = new StringBuilder();
+        
+        query.append("SELECT count(*) ");
+        query.append("FROM Member ");
+        query.append("WHERE memId = ?");
+        jdbcUtil.setSqlAndParameters(query.toString(), new Object[] {userId});
+        
+        try {
+            ResultSet rs = jdbcUtil.executeQuery();
+            if(rs.next()) {
+                int count = rs.getInt(1);
+                return (count == 1 ? true: false);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            jdbcUtil.close();
+        }
+        
+        return false;
+    }
+}
